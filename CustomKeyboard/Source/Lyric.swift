859890dfd539a9b9d8ac0b80c36d0dc9edd183aa
Luc Succes <luc@spotify.com>
2014-11-16 18:41:28 -0500

    Adds App Icon

---
+++ CustomKeyboard/Source/Lyric.swift
@@ -0,0 +1,13 @@
+//
+//  Lyric.swift
+//  DrizzyChat
+//
+//  Created by Luc Success on 11/15/14.
+//  Copyright (c) 2014 Luc Success. All rights reserved.
+//
+
+import UIKit
+
+class Lyric: NSObject {
+   
+}
