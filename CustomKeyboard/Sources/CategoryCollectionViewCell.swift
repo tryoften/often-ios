402e193049cfe60e62784d7b00a6d6f3e66d90ed
Luc Succes <luc@spotify.com>
2015-04-13 01:50:12 -0400

    Adds CategoryCollectionView classes

---
+++ CustomKeyboard/Sources/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Drizzy
+//
+//  Created by Luc Success on 4/13/15.
+//  Copyright (c) 2015 Luc Success. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
