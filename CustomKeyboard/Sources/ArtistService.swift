f099ce46b605bcffcc30ebe38a81db6d18feaaec
Luc Succes <luc@spotify.com>
2015-04-17 02:21:15 -0400

    Adds Artist Service

---
+++ CustomKeyboard/Sources/ArtistService.swift
@@ -0,0 +1,13 @@
+//
+//  ArtistService.swift
+//  Drizzy
+//
+//  Created by Luc Success on 4/15/15.
+//  Copyright (c) 2015 Luc Success. All rights reserved.
+//
+
+import UIKit
+
+class ArtistService: NSObject {
+   
+}
