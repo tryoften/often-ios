6b43c151495d89cfb42dba83e29955e93166e594
Luc Succes <luc@spotify.com>
2015-05-19 01:09:14 -0400

    Adds Tab Bar icons

---
+++ DrizzyChat/UserProfileHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  UserProfileHeaderView.swift
+//  Drizzy
+//
+//  Created by Luc Success on 5/17/15.
+//  Copyright (c) 2015 Luc Success. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileHeaderView: UICollectionReusableView {
+        
+}
---
+++ DrizzyChat/UserProfileViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserProfileViewModel.swift
+//  Drizzy
+//
+//  Created by Luc Success on 5/17/15.
+//  Copyright (c) 2015 Luc Success. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewModel: NSObject {
+   
+}
