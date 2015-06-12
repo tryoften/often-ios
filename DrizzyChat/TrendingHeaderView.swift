b4ed0da2afb0c4ee189980faece0d3419377daeb
Komran Ghahremani <gkomran@gmail.com>
2015-06-12 17:12:18 -0400

    Tending View

---
+++ DrizzyChat/TrendingHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  TrendingHeaderView.swift
+//  Drizzy
+//
+//  Created by Komran Ghahremani on 6/9/15.
+//  Copyright (c) 2015 Luc Success. All rights reserved.
+//
+
+import UIKit
+
+class TrendingHeaderView: UICollectionReusableView {
+        
+}
