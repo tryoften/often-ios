3a7799069859ca0f03b36c8a989f94a3ae9c59f4
Luc Succes <luc@drizzyapp.com>
2015-07-01 14:42:37 -0400

    Adds message bar

---
+++ CustomKeyboard/MessageBarView.swift
@@ -0,0 +1,21 @@
+//
+//  MessageBarView.swift
+//  Drizzy
+//
+//  Created by Luc Succes on 7/1/15.
+//  Copyright (c) 2015 Luc Success. All rights reserved.
+//
+
+import UIKit
+
+class MessageBarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
