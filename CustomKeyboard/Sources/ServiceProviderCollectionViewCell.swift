055c18e75cdc1c2d8ecf2942b6d6a1d8af02ade3
Komran Ghahremani <gkomran@gmail.com>
2015-07-31 18:39:09 -0400

    Collection view for content view controller and dynamic cell

---
+++ CustomKeyboard/Sources/ServiceProviderCollectionViewCell.swift
@@ -0,0 +1,126 @@
+//
+//  ServiceProviderCollectionViewCell.swift
+//  Surf
+//
+//  Created by Komran Ghahremani on 7/31/15.
+//  Copyright (c) 2015 Surf Inc. All rights reserved.
+//
+
+import UIKit
+
+class ServiceProviderCollectionViewCell: UICollectionViewCell {
+    var informationContainerView: UIView
+    var avatarImageView: UIImageView
+    var headerLabel: UILabel
+    var mainTextLabel: UILabel
+    var leftSupplementLabel: UILabel
+    var centerSupplementLabel: UILabel
+    var rightSupplementLabel: UILabel
+    var rightCornerImageView: UIImageView
+    
+    var contentImageView: UIImageView
+    var contentImageViewWidth: CGFloat = 0
+    var contentImage: UIImage {
+        didSet (value) {
+            contentImageView.image = value
+            contentImageViewWidth = 100
+        }
+    }
+    
+    override init(frame: CGRect) {
+        informationContainerView = UIView()
+        informationContainerView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        avatarImageView = UIImageView()
+        avatarImageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        avatarImageView.contentMode = .ScaleAspectFit
+        
+        headerLabel = UILabel()
+        headerLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        headerLabel.font = UIFont(name: "OpenSans", size: 12.0)
+        
+        mainTextLabel = UILabel()
+        mainTextLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        mainTextLabel.font = UIFont(name: "OpenSans", size: 12.0)
+        
+        centerSupplementLabel = UILabel()
+        centerSupplementLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        centerSupplementLabel.font = UIFont(name: "OpenSans", size: 12.0)
+        centerSupplementLabel.textColor = LightGrey
+        
+        leftSupplementLabel = UILabel()
+        leftSupplementLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        leftSupplementLabel.font = UIFont(name: "OpenSans", size: 12.0)
+        leftSupplementLabel.textColor = LightGrey
+        
+        rightSupplementLabel = UILabel()
+        rightSupplementLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        rightSupplementLabel.font = UIFont(name: "OpenSans", size: 12.0)
+        rightSupplementLabel.textColor = LightGrey
+        
+        rightCornerImageView = UIImageView()
+        rightCornerImageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        rightCornerImageView.contentMode = .ScaleAspectFit
+        
+        contentImage = UIImage()
+        
+        contentImageView = UIImageView()
+        contentImageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        contentImageView.contentMode = .ScaleAspectFit
+        
+        super.init(frame: frame)
+        
+        addSubview(informationContainerView)
+        addSubview(contentImageView)
+        informationContainerView.addSubview(avatarImageView)
+        informationContainerView.addSubview(headerLabel)
+        informationContainerView.addSubview(mainTextLabel)
+        informationContainerView.addSubview(leftSupplementLabel)
+        informationContainerView.addSubview(centerSupplementLabel)
+        informationContainerView.addSubview(rightSupplementLabel)
+        informationContainerView.addSubview(rightCornerImageView)
+        
+        setupLayout()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupLayout() {
+        addConstraints([
+            informationContainerView.al_left == al_left,
+            informationContainerView.al_top == al_top,
+            informationContainerView.al_bottom == al_bottom,
+            informationContainerView.al_right == contentImageView.al_left,
+            
+            contentImageView.al_right == al_right,
+            contentImageView.al_top == al_top,
+            contentImageView.al_bottom == al_bottom,
+            contentImageView.al_width == contentImageViewWidth,
+            
+            avatarImageView.al_left == informationContainerView.al_left + 5,
+            avatarImageView.al_top == informationContainerView.al_top + 5,
+            avatarImageView.al_width == 10,
+            avatarImageView.al_height == 10,
+            
+            headerLabel.al_left == avatarImageView.al_right + 2,
+            headerLabel.al_top == informationContainerView.al_top + 5,
+            
+            mainTextLabel.al_left == informationContainerView.al_left + 5,
+            mainTextLabel.al_top == headerLabel.al_bottom + 5,
+            
+            leftSupplementLabel.al_bottom == informationContainerView.al_bottom + 5,
+            leftSupplementLabel.al_left == informationContainerView.al_left + 5,
+            
+            centerSupplementLabel.al_left == leftSupplementLabel.al_right + 5,
+            centerSupplementLabel.al_bottom == informationContainerView.al_bottom + 5,
+            
+            rightSupplementLabel.al_left == informationContainerView.al_right - 5,
+            rightSupplementLabel.al_bottom == informationContainerView.al_bottom + 5,
+            
+            rightCornerImageView.al_top == informationContainerView.al_top + 5,
+            rightCornerImageView.al_right == contentImageView.al_left - 5
+        ])
+    }
+}
---
+++ CustomKeyboard/Sources/ServiceProviderCollectionViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ServiceProviderCollectionViewController.swift
+//  Surf
+//
+//  Created by Komran Ghahremani on 7/31/15.
+//  Copyright (c) 2015 Surf Inc. All rights reserved.
+//
+
+import UIKit
+
+/**
+    ServiceProviderCollectionViewController
+
+    Collection view that can display any type of service provider cell because they are all 
+    the same size.
+    
+    Types of providers:
+
+    Song Cell
+    Video Cell
+    Article Cell
+    Tweet Cell
+
+*/
+
+let serviceProviderSongCellReuseIdentifier = "songCell"
+let serviceProviderVideoCellReuseIdentifier = "videoCell"
+let serviceProviderArticleCellReuseIdentifier = "articleCell"
+let serviceProviderTweetCellReuseIdentifier = "tweetCell"
+
+class ServiceProviderCollectionViewController: UICollectionViewController {
+    var screenWidth = UIScreen.mainScreen().bounds.width
+    var resultsLabel: UILabel
+    
+    override init(collectionViewLayout layout: UICollectionViewLayout) {
+        resultsLabel = UILabel()
+        
+        super.init(collectionViewLayout: layout)
+        
+        view.backgroundColor = LightGrey
+        
+        setupLayout()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Register cell classes
+        if let collectionView = collectionView {
+            collectionView.registerClass(ServiceProviderSongCollectionViewCell.self, forCellWithReuseIdentifier: serviceProviderSongCellReuseIdentifier)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func provideCollectionViewFlowLayout() -> UICollectionViewFlowLayout {
+        var layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSizeMake(screenWidth, 100)
+        layout.scrollDirection = .Vertical
+        layout.minimumInteritemSpacing = 5.0
+        layout.minimumLineSpacing = 5.0
+        layout.sectionInset = UIEdgeInsets(top: 10.0, left: 5.0, bottom: 5.0, right: 5.0)
+        return layout
+    }
+    
+    func setupLayout() {
+        view.addConstraints([
+            resultsLabel.al_top == view.al_top + 3,
+            resultsLabel.al_left == view.al_left + 3
+            ])
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(serviceProviderSongCellReuseIdentifier, forIndexPath: indexPath) as! ServiceProviderSongCollectionViewCell
+        
+        cell.usernameLabel.text = "octobersveryown"
+        cell.trackLabel.text = "Back to Back Freestyle"
+        cell.playsLabel.text = "4,593,284 Plays"
+        cell.dateLabel.text = "July 25, 2015"
+        cell.trackImageView.image = UIImage(named: "")
+        
+        return cell
+    }
+}
