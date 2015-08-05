ff735a1ef5679af436e00048b0a7c27221e59a02
Luc Succes <luc@onoctober.com>
2015-08-05 16:50:07 -0400

    Swapped branding to Often

---
+++ CustomKeyboard/Sources/VenmoContactsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  VenmoContactsViewModel.swift
+//  Surf
+//
+//  Created by Luc Succes on 8/5/15.
+//  Copyright (c) 2015 Surf Inc. All rights reserved.
+//
+
+import UIKit
+
+class VenmoContactsViewModel: NSObject {
+   
+}
---
+++ Vendors/FuzzySearch.swift
@@ -0,0 +1,38 @@
+
+func fuzzySearch(var originalString: String, var stringToSearch: String, caseSensitive: Bool = true) -> Bool {
+
+    if count(originalString) == 0 || count(stringToSearch) == 0 {
+        return false
+    }
+
+    if count(originalString) < count(stringToSearch) {
+        return false
+    }
+
+    if caseSensitive {
+        originalString = originalString.lowercaseString
+        stringToSearch = stringToSearch.lowercaseString
+    }
+
+    var searchIndex: Int = 0
+
+    for charOut in originalString {
+        for (indexIn, charIn) in enumerate(stringToSearch) {
+            if indexIn == searchIndex {
+                if charOut == charIn{
+                    searchIndex++
+                    if searchIndex == count(stringToSearch) {
+                        return true
+                    } else {
+                        break
+                    }
+                }
+                else {
+                    break
+                }
+
+            }
+        }
+    }
+    return false
+}
