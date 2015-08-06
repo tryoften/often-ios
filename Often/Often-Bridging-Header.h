d3e1825bda7b0fccde0a301f81b896b814d55909
Luc Succes <luc@onoctober.com>
2015-08-05 23:45:18 -0400

    Surf -> Often

--- Often/Often-Bridging-Header.h
+++ Often/Often-Bridging-Header.h
@@ -1,9 +1,9 @@
 //
-//  DrizzyChat-Bridging-Header.h
-//  DrizzyChat
+//  Often-Bridging-Header.h
+//  Often
 //
 //  Created by Luc Success on 11/15/14.
-//  Copyright (c) 2014 Luc Success. All rights reserved.
+//  Copyright (c) 2014 Surf Inc. All rights reserved.
 //
 
 #import <AFNetworking/AFNetworking.h>
