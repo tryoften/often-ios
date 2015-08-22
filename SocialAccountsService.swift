d2ba1ae3532209307375a4c887bb7921b6152c1d
Kervins Valcourt <kervins.valcourtz@gmail.com>
2015-08-21 20:24:29 -0400

    updated the services to add and remove social accounts

--- SocialAccountsService.swift
+++ SocialAccountsService.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 class SocialAccountsService: Service {
+    let user: User
     var socialAccountsRef: Firebase
-    var socialAccounts: [SocialAccount]
-    let socialAccountsPath = "socialAccounts"
+    var socialAccounts: [String: SocialAccount] {
+        didSet {
+            var socialAccountList = sorted(self.socialAccounts.values.array) {$0.name < $1.name}
+            for var i = 0; i < socialAccountList.count; i++ {
+                socialAccountList[i].index = i
+            }
+            sortedSocialAccounts = socialAccountList
+        }
+    }
     
-    override init(root: Firebase) {
+    var sortedSocialAccounts: [SocialAccount]
+    let socialAccountsPath = "socialAccounts"
+    var isUpdatingSocialAccount = false
+    
+    init(user: User, root: Firebase) {
+        self.user = user
         socialAccountsRef = root.childByAppendingPath(socialAccountsPath)
-        socialAccounts = [SocialAccount]()
+        sortedSocialAccounts = [SocialAccount]()
+        socialAccounts = [String: SocialAccount]()
         
         super.init(root: root)
         
@@ -36,7 +50,7 @@ class SocialAccountsService: Service {
     private func createSocialAccountsModels(completion: (Bool) -> Void) {
         if let services = userDefaults.arrayForKey(socialAccountsPath)
         {
-            socialAccounts = services as! [SocialAccount]
+            sortedSocialAccounts = services as! [SocialAccount]
             delegate?.serviceDataDidLoad(self)
             completion(true)
         }
@@ -70,10 +84,10 @@ class SocialAccountsService: Service {
     func requestData(completion: ([SocialAccount]) -> Void) {
         fetchRemoteData { success in
             if success {
-                completion(self.socialAccounts)
+                completion(self.sortedSocialAccounts)
             } else {
                 self.fetchLocalData { success in
-                    completion(self.socialAccounts)
+                    completion(self.sortedSocialAccounts)
                 }
             }
         }
@@ -93,15 +107,15 @@ class SocialAccountsService: Service {
                 serviceList.append(socialAccount)
                 
                 if index + 1 >= serviceCount {
-                    self.socialAccounts = sorted(serviceList) { $0.name < $1.name }
+                    self.sortedSocialAccounts = sorted(serviceList) { $0.name < $1.name }
                     
-                    self.userDefaults.setValue(self.socialAccounts, forKey: self.socialAccountsPath)
+                    self.userDefaults.setValue(self.sortedSocialAccounts, forKey: self.socialAccountsPath)
                     self.userDefaults.synchronize()
     
                     
                     dispatch_async(dispatch_get_main_queue(), {
                         self.delegate?.serviceDataDidLoad(self)
-                        completion(self.socialAccounts)
+                        completion(self.sortedSocialAccounts)
                     })
                 }
             })
@@ -114,8 +128,8 @@ class SocialAccountsService: Service {
     :param: serviceId The id from the key/value store, the SocialAccount object ID
     :param: completion callback which gets called when service objects are done being created
     */
-    func processSocialAccountsData(serviceId: String, completion: (SocialAccount, Bool) -> ()) {
-        let serviceRef = rootURL.childByAppendingPath("socialAccounts/\(serviceId)")
+    func processSocialAccountsData(socialAccountId: String, completion: (SocialAccount, Bool) -> ()) {
+        let serviceRef = rootURL.childByAppendingPath("socialAccounts/\(socialAccountId)")
         
         serviceRef.observeSingleEventOfType(.Value, withBlock: { (snapshot) in
             if let data = snapshot.value as? [String: AnyObject]
@@ -124,6 +138,65 @@ class SocialAccountsService: Service {
                     service.setValuesForKeysWithDictionary(data)
                     completion(service, true)
             }
+        })
+    }
+    
+    /**
+    unsubscribe a Social Account with the given id
+    
+    :param: socialAccountId the id of the social Account to delete
+    :param: completion callback when the delete operation has been successfully persisted on the backend
+    */
+    func removeSocialAccounteWithId(socialAccountId: String, completion: (NSError?) -> ()) {
+        if isUpdatingSocialAccount {
+            return
+        }
+        isUpdatingSocialAccount = true
+        let socialAccount = socialAccounts[socialAccountId]
+        socialAccounts.removeValueForKey(socialAccountId)
+        
+        
+        if let socialAccount = socialAccount {
+                NSNotificationCenter.defaultCenter().postNotificationName("socialAccount:removed", object: self, userInfo: [
+                    "socialAccountId": socialAccount.id,
+                    "index": socialAccount.index
+                    ])
+            self.userDefaults.setValue(socialAccount, forKey: self.socialAccountsPath)
+            self.userDefaults.synchronize()
+                self.isUpdatingSocialAccount = false
+        }
+        
+        
+        self.socialAccountsRef.childByAppendingPath(socialAccountId).removeValueWithCompletionBlock { (err, socialAccountsRef) in
+            completion(nil)
+        }
+    }
+    
+    func addSocialAccounteWithId(socialAccountId: String, completion: (SocialAccount, Bool) -> ()) {
+        if let socialAccount = socialAccounts[socialAccountId] {
+            completion(socialAccount, true)
+            NSNotificationCenter.defaultCenter().postNotificationName("socialAccount:added", object: self, userInfo: [
+                "socialAccountId": socialAccount.id,
+                "index": socialAccount.index
+                ])
+            return
+        }
+        if isUpdatingSocialAccount {
+            return
+        }
+        isUpdatingSocialAccount = true
+        processSocialAccountsData(socialAccountId, completion: { (socialAccount, success) in
+            self.socialAccountsRef.childByAppendingPath(socialAccount.id).setValue(true)
+            
+            socialAccount.user = self.user
+            self.socialAccounts[socialAccount.id] = socialAccount
+            completion(socialAccount, success)
+            NSNotificationCenter.defaultCenter().postNotificationName("keyboard:added", object: self, userInfo: [
+                "socialAccountId": socialAccount.id,
+                "index": socialAccount.index
+                ])
+            self.isUpdatingSocialAccount = false
+            
         })
     }
 }
