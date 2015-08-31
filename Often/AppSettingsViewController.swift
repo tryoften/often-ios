149ed6a5a90801095119c71c8b97abdfb440b594
Komran Ghahremani <gkomran@gmail.com>
2015-08-30 21:33:29 -0400

    settings table view unstyled

--- Often/AppSettingsViewController.swift
+++ Often/AppSettingsViewController.swift
@@ -9,29 +9,45 @@
 import UIKit
 
 class AppSettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var tableView: UITableView
-    var settingOptions: NSDictionary = [
-        "Account": ["Name", "Email", "Password", "Push Notifications"],
-        "Actions": ["How To Install Often", "Rate In App Store", "Support"],
-        "About": ["FAQ", "Privacy Policy", "Terms Of Use", "Licenses"]
+    var containerView: UIView
+    var tableView: UITableView?
+    
+    var accountSettings = [
+        "Name",
+        "Email",
+        "Password",
+        "Push Notifications"
     ]
     
-    var sectionHeaders: NSArray = [
-        "Account",
-        "Actions",
-        "About"
+    var actionsSettings = [
+        "How to Install Often",
+        "Rate in App Store",
+        "Support"
+    ]
+    
+    var aboutSettings = [
+        "FAQ",
+        "Privacy Policy",
+        "Terms of Use",
+        "Licenses"
     ]
     
     init() {
-        tableView = UITableView()
-        tableView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        containerView = UIView()
+        containerView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        containerView.backgroundColor = UIColor.grayColor()
         
         super.init(nibName: nil, bundle: nil)
         
-        tableView.delegate = self
-        tableView.dataSource = self
+        tableView = UITableView(frame: view.bounds, style: .Grouped)
+        if let tableView = tableView {
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "settingCell")
+            containerView.addSubview(tableView)
+        }
         
-        view.addSubview(tableView)
+        view.addSubview(containerView)
         
         setupLayout()
     }
@@ -42,8 +58,6 @@ class AppSettingsViewController: UIViewController, UITableViewDataSource, UITabl
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,26 +65,55 @@ class AppSettingsViewController: UIViewController, UITableViewDataSource, UITabl
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Account"
+        } else if section == 1 {
+            return "Actions"
+        } else {
+            return "About"
+        }
+    }
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return 3
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if section == 0 {
+            return accountSettings.count
+        } else if section == 1 {
+            return actionsSettings.count
+        } else {
+            return aboutSettings.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("settingCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = accountSettings[indexPath.row]
+        } else if indexPath.section == 1 {
+            cell.textLabel?.text = actionsSettings[indexPath.row]
+        } else {
+            cell.textLabel?.text = aboutSettings[indexPath.row]
+        }
         
         return cell
     }
     
     func setupLayout() {
         view.addConstraints([
-            tableView.al_left == view.al_left,
-            tableView.al_top == view.al_top,
-            tableView.al_bottom == view.al_bottom,
-            tableView.al_right == view.al_right
+            tableView!.al_left == containerView.al_left,
+            tableView!.al_top == containerView.al_top,
+            tableView!.al_bottom == containerView.al_bottom,
+            tableView!.al_right == containerView.al_right,
+            
+            containerView.al_left == view.al_left + 50,
+            containerView.al_top == view.al_top,
+            containerView.al_right == view.al_right,
+            containerView.al_bottom == view.al_bottom
         ])
     }
 }
