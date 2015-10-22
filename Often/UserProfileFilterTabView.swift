361552abe1784500eb5329be9b0202444fd03ffd
Komran Ghahremani <gkomran@gmail.com>
2015-10-22 19:53:14 -0400

    Enum Fix

--- Often/UserProfileFilterTabView.swift
+++ Often/UserProfileFilterTabView.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class UserProfileFilterTabView: UIView {
+    
+    enum FilterType {
+        case All
+        case Songs
+        case Videos
+        case Links
+        case Gifs
+    }
+    
+    var currentFilter: FilterType = .All
+    
     let allFilterButton: UIButton
     let songsFilterButton: UIButton
     let videosFilterButton: UIButton
@@ -126,73 +137,87 @@ class UserProfileFilterTabView: UIView {
     //Filter Method
     func filterButtonTapped(button: UIButton) {
         let buttonWidth = UIScreen.mainScreen().bounds.width / 5
+        var type = currentFilter
+        if button.titleLabel?.text == "ALL" {
+            type = .All
+        } else if button.titleLabel?.text == "SONGS" {
+            type = .Songs
+        } else if  button.titleLabel?.text == "VIDEOS" {
+            type = .Videos
+        } else if  button.titleLabel?.text == "LINKS" {
+            type = .Links
+        } else if  button.titleLabel?.text == "GIFS" {
+            type = .Gifs
+        }
         
-        if let title = button.titleLabel?.text {
-            switch title {
-            case "ALL":
-                if allFilterButton.selected == true {
-                    //do nothing
-                } else {
-                    allFilterButton.selected = true
-                    songsFilterButton.selected = false
-                    videosFilterButton.selected = false
-                    linksFilterButton.selected = false
-                    gifsFilterButton.selected = false
-                    highlightBarLeftConstraint?.constant = 0.0
-                }
-                break
-            case "SONGS":
-                if songsFilterButton.selected == true {
-                    //do nothing
-                } else {
-                    allFilterButton.selected = false
-                    songsFilterButton.selected = true
-                    videosFilterButton.selected = false
-                    linksFilterButton.selected = false
-                    gifsFilterButton.selected = false
-                    highlightBarLeftConstraint?.constant = buttonWidth
-                }
-                break
-            case "VIDEOS":
-                if videosFilterButton.selected == true {
-                    //do nothing
-                } else {
-                    allFilterButton.selected = false
-                    songsFilterButton.selected = false
-                    videosFilterButton.selected = true
-                    linksFilterButton.selected = false
-                    gifsFilterButton.selected = false
-                    highlightBarLeftConstraint?.constant = buttonWidth * 2
-                }
-                break
-            case "LINKS":
-                if linksFilterButton.selected == true {
-                    //do nothing
-                } else {
-                    allFilterButton.selected = false
-                    songsFilterButton.selected = false
-                    videosFilterButton.selected = false
-                    linksFilterButton.selected = true
-                    gifsFilterButton.selected = false
-                    highlightBarLeftConstraint?.constant = buttonWidth * 3
-                }
-                break
-            case "GIFS":
-                if gifsFilterButton.selected == true {
-                    //do nothing
-                } else {
-                    allFilterButton.selected = false
-                    songsFilterButton.selected = false
-                    videosFilterButton.selected = false
-                    linksFilterButton.selected = false
-                    gifsFilterButton.selected = true
-                    highlightBarLeftConstraint?.constant = buttonWidth * 4
-                }
-                break
-            default:
-                print("Defaulted")
-                break
+        
+        switch type {
+        case .All:
+            if allFilterButton.selected == true {
+                //do nothing
+            } else {
+                allFilterButton.selected = true
+                songsFilterButton.selected = false
+                videosFilterButton.selected = false
+                linksFilterButton.selected = false
+                gifsFilterButton.selected = false
+                highlightBarLeftConstraint?.constant = 0.0
+                currentFilter = .All
             }
+            break
+        case .Songs:
+            if songsFilterButton.selected == true {
+                //do nothing
+            } else {
+                allFilterButton.selected = false
+                songsFilterButton.selected = true
+                videosFilterButton.selected = false
+                linksFilterButton.selected = false
+                gifsFilterButton.selected = false
+                highlightBarLeftConstraint?.constant = buttonWidth
+                currentFilter = .Songs
+            }
+            break
+        case .Videos:
+            if videosFilterButton.selected == true {
+                //do nothing
+            } else {
+                allFilterButton.selected = false
+                songsFilterButton.selected = false
+                videosFilterButton.selected = true
+                linksFilterButton.selected = false
+                gifsFilterButton.selected = false
+                highlightBarLeftConstraint?.constant = buttonWidth * 2
+                currentFilter = .Videos
+            }
+            break
+        case .Links:
+            if linksFilterButton.selected == true {
+                //do nothing
+            } else {
+                allFilterButton.selected = false
+                songsFilterButton.selected = false
+                videosFilterButton.selected = false
+                linksFilterButton.selected = true
+                gifsFilterButton.selected = false
+                highlightBarLeftConstraint?.constant = buttonWidth * 3
+                currentFilter = .Links
+            }
+            break
+        case .Gifs:
+            if gifsFilterButton.selected == true {
+                //do nothing
+            } else {
+                allFilterButton.selected = false
+                songsFilterButton.selected = false
+                videosFilterButton.selected = false
+                linksFilterButton.selected = false
+                gifsFilterButton.selected = true
+                highlightBarLeftConstraint?.constant = buttonWidth * 4
+                currentFilter = .Gifs
+            }
+            break
         }
     }
 }
+
