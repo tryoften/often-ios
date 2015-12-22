247a3c655d2ed188851a578711c405a28e875ef1
Luc Succes <luc@onoctober.com>
2015-12-22 18:40:24 -0500

    Cleans up/refactor search text field code

---
+++ Keyboard/Sources/KeyboardSearchBarController.swift
@@ -0,0 +1,69 @@
+//
+//  KeyboardSearchBarController.swift
+//  Often
+//
+//  Created by Luc Succes on 12/22/15.
+//  Copyright © 2015 Surf Inc. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardSearchBarController: SearchBarController {
+    var primaryTextDocumentProxy: UITextDocumentProxy?
+
+    weak var textProcessor: TextProcessingManager? {
+        didSet {
+            primaryTextDocumentProxy = textProcessor?.currentProxy
+
+            if let textProcessor = textProcessor, textInput = searchBar.textInput as? KeyboardSearchTextField {
+                textProcessor.proxies["search"] = textInput
+            }
+        }
+    }
+
+    required init(viewModel aViewModel: SearchViewModel, suggestionsViewModel aSuggestionsViewModel: SearchSuggestionsViewModel, SearchTextFieldClass: SearchTextField.Type) {
+        super.init(viewModel: aViewModel, suggestionsViewModel: aSuggestionsViewModel, SearchTextFieldClass: KeyboardSearchTextField.self)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func textFieldDidBeginEditing(textField: UITextField) {
+        guard let textInput = searchBar.textInput as? KeyboardSearchTextField else {
+            return
+        }
+
+        textProcessor?.setCurrentProxyWithId("search")
+        scheduleAutocompleteRequestTimer()
+
+        let center = NSNotificationCenter.defaultCenter()
+        center.postNotificationName(RestoreKeyboardEvent, object: nil)
+        center.postNotificationName(ToggleButtonKeyboardEvent, object: nil, userInfo: ["hide": true])
+
+        delay(0.5) {
+            center.postNotificationName(TextProcessingManagerProxyEvent, object: self, userInfo: [
+                "id": textInput.id,
+                "setDefault": true
+            ])
+        }
+    }
+
+    func textFieldDidChange() {
+        textProcessor?.parseTextInCurrentDocumentProxy()
+
+        if viewModel.hasReceivedResponse == true {
+            scheduleAutocompleteRequestTimer()
+            NSNotificationCenter.defaultCenter().postNotificationName(ToggleButtonKeyboardEvent, object: nil, userInfo: ["hide": true])
+        }
+    }
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        guard let textInput = searchBar.textInput as? KeyboardSearchTextField else {
+            return
+        }
+
+        textProcessor?.setCurrentProxyWithId("default")
+        textInput.repositionText()
+    }
+}
