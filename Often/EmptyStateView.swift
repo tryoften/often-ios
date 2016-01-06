c8b71db2375f72e241926897b2c17289383c542c
Komran Ghahremani <gkomran@gmail.com>
2016-01-06 15:35:45 -0500

    Spacing

--- Often/EmptyStateView.swift
+++ Often/EmptyStateView.swift
@@ -15,7 +15,6 @@ class EmptyStateView: UIView {
     var primaryButton: UIButton
     var secondaryButton: UIButton?
     var closeButton: UIButton
-    
     var imageSize: EmptyStateImageSize {
         didSet {
             switch self.imageSize {
