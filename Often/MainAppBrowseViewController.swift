fc50b92cb30499db33d626c99a853072f9208ac3
Kervins Valcourt <kervins.valcourtz@gmail.com>
2016-01-07 22:15:01 -0500

    add main app search bar styling to its class instead of doing in in the view controller

--- Often/MainAppBrowseViewController.swift
+++ Often/MainAppBrowseViewController.swift
@@ -59,24 +59,8 @@ class MainAppBrowseViewController: BrowseViewController, BrowseHeaderViewDelegat
         searchViewController.searchResultsViewController.contentInset = mainAppSearchResultsCollectionViewControllerContentInsets
 
         self.searchBar = searchBar
-
-        let attributes: [String: AnyObject] = [
-            NSFontAttributeName: UIFont(name: "Montserrat", size: 11)!,
-            NSForegroundColorAttributeName: BlackColor
-        ]
-
+        
         navigationItem.titleView = searchBar
-        searchBar.searchBarStyle = .Minimal
-        searchBar.backgroundColor = WhiteColor
-        searchBar.tintColor = UIColor(fromHexString: "#14E09E")
-        searchBar.placeholder = SearchBarPlaceholderText
-        searchBar.setValue("cancel".uppercaseString, forKey:"_cancelButtonText")
-
-        if #available(iOS 9.0, *) {
-            UIBarButtonItem.appearanceWhenContainedInInstancesOfClasses([MainAppSearchBar.self]).setTitleTextAttributes(attributes, forState: .Normal)
-        } else {
-            UIBarButtonItem.appearance().setTitleTextAttributes(attributes, forState: .Normal)
-        }
 
         addChildViewController(searchViewController)
         view.addSubview(searchViewController.view)
