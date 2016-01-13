8067fa21cee6f3785427c78e06ae70bb91c7794e
Komran Ghahremani <gkomran@gmail.com>
2016-01-12 23:02:52 -0500

    No Results Try Again Style & Implementation

--- Often/NoResultsEmptyStateView.swift
+++ Often/NoResultsEmptyStateView.swift
@@ -16,12 +16,25 @@ class NoResultsEmptyStateView: EmptyStateView {
         
         super.init(frame: CGRectZero)
         
+        primaryButton.removeFromSuperview()
+        
+        primaryButton = UIButton()
+        primaryButton.translatesAutoresizingMaskIntoConstraints = false
+        primaryButton.titleLabel!.font = UIFont(name: "Montserrat", size: 11)
+        primaryButton.clipsToBounds = true
+        primaryButton.layer.cornerRadius = 20
+        primaryButton.backgroundColor = TealColor
+        primaryButton.setTitle("try again".uppercaseString, forState: .Normal)
+        primaryButton.hidden = false
+        
         titleLabel.text = title
         descriptionLabel.text = description
         imageView.image = image
         
         imageView.contentMode = .ScaleAspectFill
         imageSize = .Large
+        
+        addSubview(primaryButton)
         
         addAdditionalLayouts()
     }
@@ -32,6 +45,15 @@ class NoResultsEmptyStateView: EmptyStateView {
     
     func addAdditionalLayouts() {
         imageViewTopConstraint?.constant = -(imageViewTopPadding + 14)
+        
+        addConstraints([
+            primaryButton.al_centerX == al_centerX,
+            primaryButton.al_top == descriptionLabel.al_bottom + 20,
+            primaryButton.al_left == al_centerX - 90,
+            primaryButton.al_right == al_centerX + 90,
+            primaryButton.al_height == 40
+        ])
+        
         layoutIfNeeded()
     }
 }
