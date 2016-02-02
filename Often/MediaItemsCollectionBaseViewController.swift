bbff513a644a0928c8d0eda80f890854776f2f6c
Kervins Valcourt <kervins.valcourtz@gmail.com>
2016-02-01 19:56:21 -0500

    disable scrolling when empty state is shown

--- Often/MediaItemsCollectionBaseViewController.swift
+++ Often/MediaItemsCollectionBaseViewController.swift
@@ -112,6 +112,8 @@ class MediaItemsCollectionBaseViewController: FullScreenCollectionViewController
     }
 
     func showEmptyStateViewForState(state: UserState, animated: Bool = false, completion: ((EmptyStateView) -> Void)? = nil) {
+        collectionView?.scrollEnabled = false
+        
         emptyStateView?.removeFromSuperview()
 
         guard state != .NonEmpty else {
