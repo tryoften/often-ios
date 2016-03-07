3b4bc9df5e8d69e9e6219c823abed92ea40d0041
Luc Succes <luc@onoctober.com>
2016-03-07 16:17:35 -0500

    Removes unused file

--- Keyboard/Sources/KeyboardBrowseMediaItemViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  KeyboardBrowseMediaItemViewController.swift
-//  Often
-//
-//  Created by Luc Succes on 3/7/16.
-//  Copyright © 2016 Surf Inc. All rights reserved.
-//
-
-import Foundation
