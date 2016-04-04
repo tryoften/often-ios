1103ed05357a4f39a915f3921ae97212c4a77337
Kervins Valcourt <kervins.valcourtz@gmail.com>
2016-04-04 00:03:30 -0400

    added categoryPanel to PackItemViewController

--- Core/BrowsePackItemViewController.swift
+++ Core/BrowsePackItemViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 private let PackPageHeaderViewIdentifier = "packPageHeaderViewIdentifier"
 
-class BrowsePackItemViewController: BrowseMediaItemViewController {
+class BrowsePackItemViewController: BrowseMediaItemViewController, KeyboardMediaItemPackPickerViewControllerDelegate {
 
     var pack: PackMediaItem? {
         didSet {
@@ -19,34 +19,43 @@ class BrowsePackItemViewController: BrowseMediaItemViewController {
                 self.collectionView?.reloadData()
             }
             headerViewDidLoad()
+            populatePanelMetaData(pack?.name, itemCount: pack?.items.count, imageUrl: pack?.smallImageURL)
         }
     }
     
     var packId: String
     
-    init(packId: String, viewModel: BrowseViewModel) {
+    init(packId: String, viewModel: BrowseViewModel, textProcessor: TextProcessingManager?) {
         self.packId = packId
         super.init(viewModel: viewModel)
+        self.textProcessor = textProcessor
         
         #if KEYBOARD
-            collectionView?.contentInset = UIEdgeInsetsMake(-35, 0, 0, 0)
+            collectionView?.contentInset = UIEdgeInsetsMake(0, 0, SectionPickerViewHeight, 0)
+
+            if let navigationBar = navigationBar {
+                navigationBar.removeFromSuperview()
+
+            }
         #else
             hudTimer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "showHud", userInfo: nil, repeats: false)
+            collectionView?.registerClass(PackPageHeaderView.self, forSupplementaryViewOfKind: CSStickyHeaderParallaxHeader, withReuseIdentifier: PackPageHeaderViewIdentifier)
         #endif
-        
-        collectionView?.registerClass(PackPageHeaderView.self, forSupplementaryViewOfKind: CSStickyHeaderParallaxHeader, withReuseIdentifier: PackPageHeaderViewIdentifier)
+
     }
     
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    
     
     override class func provideCollectionViewLayout() -> UICollectionViewFlowLayout {
         let screenWidth = UIScreen.mainScreen().bounds.size.width
         
         #if KEYBOARD
-            let topMargin = CGFloat(115.0)
+            let topMargin = CGFloat(0)
             let layout = UICollectionViewFlowLayout()
         #else
             let topMargin = CGFloat(10.0)
@@ -67,10 +76,10 @@ class BrowsePackItemViewController: BrowseMediaItemViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         pack = nil
         collectionView?.reloadData()
-        
+
         viewModel.getPackWithOftenId(packId) { model in
             self.pack = model
             
@@ -80,14 +89,34 @@ class BrowsePackItemViewController: BrowseMediaItemViewController {
         }
         
     }
-    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupCategoryCollectionViewController()
+        layoutCategoryPanelView()
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        HUDMaskView?.frame = view.bounds
+
+        guard let categoriesVC = categoriesVC where !categoriesVC.panelView.isOpened else {
+            return
+        }
+
+        layoutCategoryPanelView()
+    }
+
+
     override func headerViewDidLoad() {
         let imageURL: NSURL? = pack?.image_url
         let subtitle: String? = pack?.description
         
         setupHeaderView(imageURL, title: pack?.name, subtitle: subtitle)
     }
-    
+
     override func setupHeaderView(imageURL: NSURL?, title: String?, subtitle: String?) {
         #if KEYBOARD
             navigationBar?.imageURL = imageURL
@@ -145,12 +174,37 @@ class BrowsePackItemViewController: BrowseMediaItemViewController {
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell: MediaItemCollectionViewCell
-        
         cell = parseMediaItemData(pack?.items, indexPath: indexPath, collectionView: collectionView) as MediaItemCollectionViewCell
         cell.style = .Cell
         cell.type = .NoMetadata
+
         animateCell(cell, indexPath: indexPath)
+
         return cell
     }
-    
+
+
+    override func togglePack() {
+        let packsVC = KeyboardMediaItemPackPickerViewController(viewModel: PacksViewModel())
+        packsVC.delegate = self
+        packsVC.transitioningDelegate = self
+        packsVC.modalPresentationStyle = .Custom
+        presentViewController(packsVC, animated: true, completion: nil)
+    }
+
+    func keyboardMediaItemPackPickerViewControllerDidSelectPack(packPicker: KeyboardMediaItemPackPickerViewController, pack: PackMediaItem) {
+        self.pack = nil
+        packId = pack.id
+
+        collectionView?.reloadData()
+
+        viewModel.getPackWithOftenId(packId) { model in
+            self.pack = model
+
+            #if !(KEYBOARD)
+                self.hideHud()
+            #endif
+        }
+    }
+
 }
