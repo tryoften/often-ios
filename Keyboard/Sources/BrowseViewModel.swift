2fe7f1eb10866e3f5c9611cc8ad7e161ee215411
Kervins Valcourt <kervins.valcourtz@gmail.com>
2016-04-07 02:13:19 -0400

    adding of filtering to viewModel

--- Keyboard/Sources/BrowseViewModel.swift
+++ Keyboard/Sources/BrowseViewModel.swift
@@ -9,8 +9,17 @@
 import Foundation
 
 class BrowseViewModel: MediaItemGroupViewModel {
+    var filteredMediaItems: [MediaItem]
+    var filters: [String: FavoritesFilter] = [:]
+    var mediaItems: [MediaItem]
+    let didChangeMediaItems = Event<[MediaItem]>()
+    
     init(path: String = "trending") {
+        filteredMediaItems = []
+        mediaItems = []
+
         super.init(baseRef: Firebase(url: BaseURL), path: path)
+
     }
 
     func getArtistWithOftenId(oftenId: String, completion: (ArtistMediaItem) -> ()) {
@@ -42,7 +51,104 @@ class BrowseViewModel: MediaItemGroupViewModel {
                 return
             }
             let pack = PackMediaItem(data: value)
+            self.mediaItems = pack.items
+            self.filteredMediaItems = pack.items
+            print(self.filteredMediaItems.count)
             completion(pack)
         })
     }
+
+    func applyFilter(filter: FavoritesFilter) {
+        filteredMediaItems = []
+        filters[filter.type] = filter
+        generateMediaItemGroups()
+        didChangeMediaItems.emit(self.mediaItems)
+    }
+
+    func generateMediaItemGroups() -> [MediaItemGroup] {
+        if !mediaItems.isEmpty {
+            return separateGroupByArtists(mediaItems)
+        }
+        return []
+    }
+
+    private func separateGroupByArtists(items: [MediaItem]) -> [MediaItemGroup] {
+        if filteredMediaItems != items {
+
+            for item in items {
+                guard let lyric = item as? MediaItem else {
+                        continue
+                }
+
+                // Check for filters, if present applies them
+                if shouldFilterLyric(lyric) {
+                    continue
+                }
+
+                 filteredMediaItems.append(lyric)
+
+            }
+
+        }
+
+        return mediaItemGroups
+    }
+
+    private func shouldFilterLyric(lyric: MediaItem) -> Bool {
+        for filter in filters.values {
+            switch filter {
+            case .artist(let artist):
+                    return true
+    
+            case .category(let category):
+                guard let lyricCategory = lyric.category else {
+                    if category.id == Category.all.id {
+                        return false
+                    }
+                    return true
+                }
+
+                if category.id == Category.all.id {
+                    return false
+                }
+
+                if lyricCategory.id != category.id {
+                    return true
+                }
+            }
+        }
+        
+        return false
+    }
+
+    private func sortLyricsByTracks(group: [MediaItem]) -> [MediaItem] {
+        var sortedTracks: [String: [MediaItem]] = [:]
+        for item in group {
+            if let lyric = item as? LyricMediaItem, let title = lyric.track_title {
+                if let _ = sortedTracks[title] {
+                    sortedTracks[title]!.append(lyric)
+                } else {
+                    sortedTracks[title] = [lyric]
+                }
+            }
+        }
+        var keys: [String] = []
+        for key in sortedTracks.keys {
+            if let tracks = sortedTracks[key] as? [LyricMediaItem] {
+                sortedTracks[key] = tracks.sort({ $0.index < $1.index })
+            }
+            keys.append(key)
+        }
+        let sortedKeys = keys.sort({$0 < $1})
+
+        var sortedGroup: [MediaItem] = []
+        for key in sortedKeys {
+            if let tracks = sortedTracks[key] {
+                sortedGroup.appendContentsOf(tracks)
+            }
+        }
+
+        return sortedGroup
+    }
+
 }
