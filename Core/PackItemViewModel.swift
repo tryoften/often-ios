755231bdfa8251cd9073000e0513e1b32e8fc6a7
Luc Succes <luc@onoctober.com>
2016-04-21 23:48:23 -0400

    Gifs rendering seperately

--- Core/PackItemViewModel.swift
+++ Core/PackItemViewModel.swift
@@ -14,8 +14,10 @@ class PackItemViewModel: BrowseViewModel {
             ref = baseRef.childByAppendingPath("packs/\(packId)")
         }
     }
-    var pack: PackMediaItem?
     
+    var pack: PackMediaItem?
+    var typeFilter: MediaType = .Gif
+
     init(packId: String) {
         self.packId = packId
         super.init(path: "packs/\(packId)")
@@ -29,5 +31,14 @@ class PackItemViewModel: BrowseViewModel {
                 self.delegate?.mediaItemGroupViewModelDataDidLoad(self, groups: self.mediaItemGroups)
             }
         })
+    }
+
+    func getMediaItemGroupForCurrentType() -> MediaItemGroup? {
+        for group in mediaItemGroups {
+            if group.type == typeFilter {
+                return group
+            }
+        }
+        return nil
     }
 }
