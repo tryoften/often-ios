b7d7e31c9b5b93b98348c391a3224ef32c06627b
Kervins Valcourt <kervins.valcourtz@gmail.com>
2016-08-05 09:17:12 -0400

    place back logic because it was being used is session manger. but I suggest to remove logic from here.

--- Core/PacksService.swift
+++ Core/PacksService.swift
@@ -295,4 +295,72 @@ class PacksService: PackItemViewModel {
             
         }
     }
+    
+    func generateSuggestedUsername() -> String {
+        var username: String = ""
+        
+        if let user = currentUser {
+            if !user.username.isEmpty {
+                username = user.username
+            } else {
+                username = user.name.stringByReplacingOccurrencesOfString(" ", withString: ".").lowercaseString
+            }
+            
+        }
+        
+        return username
+    }
+    
+    func usernameDoesExist(username: String, completion: ((Bool) -> Void)? = nil) {
+        let baseRef = FIRDatabase.database().reference()
+        let encodedString = encodeString(username)
+        let ref = baseRef.child("usernames/\(encodedString)")
+        var exists: Bool = true
+        
+        ref.observeEventType(.Value, withBlock: { snapshot in
+            
+            if let _ = snapshot.value as? NSDictionary {
+                exists = true
+            } else {
+                exists = false
+            }
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                completion?(exists)
+            }
+        })
+        
+    }
+    
+    func saveUsername(username: String) {
+        guard let id = currentUser?.id else {
+            return
+        }
+        
+        currentUser?.username = username
+        
+        let baseRef = FIRDatabase.database().reference()
+        let encodedString = encodeString(username)
+        var ref = baseRef.child("usernames/\(encodedString)")
+        ref.setValue([
+            "name": username,
+            "userid": id,
+            "packid": packId
+            ])
+        
+        ref = baseRef.child("users/\(id)")
+        ref.updateChildValues([
+            "username": username
+            ])
+        
+    }
+    
+    func encodeString(username: String) -> String {
+        let plainData = (username as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+        if let base64String = plainData?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0)) {
+            return base64String
+        }
+        return ""
+    }
+    
 }
