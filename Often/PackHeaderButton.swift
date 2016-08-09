bd2491295134170fb104a1366ace492dd4ab3ec3
kcfin <kate@tryoften.com>
2016-08-09 18:10:15 -0400

    trying to fix merge conflict....

---
+++ Often/PackHeaderButton.swift
@@ -0,0 +1,33 @@
+//
+//  PackHeaderButton.swift
+//  Often
+//
+//  Created by Katelyn Findlay on 8/9/16.
+//  Copyright © 2016 Surf Inc. All rights reserved.
+//
+
+import Foundation
+
+class PackHeaderButton: UIButton {
+    var textLabel: UILabel
+    //    var buttonImageView: UIImageView
+    
+    var text: String? {
+        didSet {
+            textLabel.setTextWith(UIFont(name: "Montserrat", size: 10.5)!, letterSpacing: 1.0, color: WhiteColor, text: text!.uppercaseString)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        textLabel = UILabel()
+        textLabel.textAlignment = .Left
+        textLabel.backgroundColor = ClearColor
+        
+        super.init(frame: frame)
+        addSubview(textLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
