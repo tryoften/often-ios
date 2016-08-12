77a9e29dc96285c83e933de8799a8e7762b66b47
kcfin <kate@tryoften.com>
2016-08-12 18:41:31 -0400

    Fix username for people who sign up with email

--- Often/UsernameViewModel.swift
+++ Often/UsernameViewModel.swift
@@ -20,7 +20,12 @@ class UsernameViewModel: BaseViewModel {
 
         if let user = currentUser {
             if !user.username.isEmpty {
-                username = user.username
+                if user.username.containsString("@") {
+                    let components = user.username.componentsSeparatedByString("@")
+                    username = components[0]
+                } else {
+                    username = user.username
+                }
             } else {
                 username = user.name.stringByReplacingOccurrencesOfString(" ", withString: ".").lowercaseString
             }
